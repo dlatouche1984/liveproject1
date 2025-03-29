9abc38404b4f42a72d2f81e084c837d5a6703f3f
Daniel Latouche <Daniel.latouche@hotmail.com>
2025-03-29 13:06:29 -0400

    POM setup First Draft - basePage, config.properties and homePage

---
+++ src/main/java/base/basePage.java
@@ -0,0 +1,43 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+//This is the base page which will be reused for all tests
+
+public class basePage {
+    public static WebDriver driver;
+    private String url;
+    private Properties prop;
+
+    public basePage() throws IOException {
+        prop = new Properties();
+        FileInputStream data = new FileInputStream("/Users/plakata/IdeaProjects/liveproject1/src/main/resources/config.properties");
+        prop.load(data);
+
+    }
+
+    public WebDriver getDriver() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        return driver;
+    }
+
+    public String getUrl() throws IOException {
+        url = prop.getProperty("url");
+        return url;
+
+    }
+    public WebElement getCookie() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement();
+    }
+}
---
+++ src/main/java/pageObject/homePage.java
@@ -0,0 +1,88 @@
+package pageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+// This page will list elements we need for the HomePage test
+public class homePage {
+
+    // initiate the WebDriver and call it driver
+    public WebDriver driver;
+
+    //  We create all WebElements for the homepage to make it easier to refer too
+
+    By toggle = By.cssSelector(".toggle");
+    By homepage_Link = By.linkText("Homepage");
+    By accordion = By.cssSelector("Accordion");
+    By browser_Tabs = By.linkText("Browser Tabs");
+    By buttons = By.linkText("Buttons");
+    By calculator = By.linkText("Calculator (JS)");
+    By contact_Us = By.linkText("Contact Us Form Test");
+    By date_Picker = By.linkText("Date Picker");
+    By dropdown_Checkbox_Radio = By.cssSelector("a[href='dropdown.html']");
+    By file_Upload = By.cssSelector("a[href='fileupload.html']");
+    By hidden_Elements = By.cssSelector("a[href='hiddenElements.html']");
+    By iframes = By.cssSelector("a[href='iframes.html']");
+    By loader = By.cssSelector("a[href='loader.html']");
+    By loader_Two = By.cssSelector("a[href='loadertwo.html']");
+    By login_Portal_test = By.cssSelector("a[href='loginPortal.html']");
+    By mouse_Movement = By.cssSelector("a[href='mouse.html']");
+    By pop_Up_Alerts = By.cssSelector("a[href='popups.html']");
+    By predictive_Search = By.cssSelector("a[href='predictive.html']");
+    By tables = By.cssSelector("a[href='tables.html']");
+    By test_Store = By.linkText("Test Store");
+    By about_Me = By.cssSelector("a[href='aboutMe.html']");
+
+
+    // Create a homePage constructor - We initiate the WebDriver and call it driver -
+    public homePage(WebDriver driver){
+        this.driver = driver;
+    }
+    // We then use the driver  to create WebElement functions so that we can call for any Test
+    public WebElement getToggle(){
+        return driver.findElement(toggle);
+    }
+    public WebElement homePage_Link(){
+        return driver.findElement(homepage_Link);
+    }
+    public WebElement accordion() {
+        return driver.findElement(accordion);
+    }
+    public WebElement browser_Tabs() {
+        return driver.findElement(browser_Tabs);
+    }
+    public WebElement buttons() {
+        return driver.findElement(buttons);
+    }
+    public WebElement calculator() {
+        return driver.findElement(calculator);
+    }
+    public WebElement contact_Us() {
+        return driver.findElement(contact_Us);
+    }
+    public WebElement date_Picker() {
+        return driver.findElement(date_Picker);
+    }
+    public WebElement dropdown_Checkbox_Radio() {
+        return driver.findElement(dropdown_Checkbox_Radio);
+    }
+    public WebElement file_Upload() {
+        return driver.findElement(file_Upload);
+    }
+    public WebElement hidden_Elements() {
+        return driver.findElement(hidden_Elements);
+    }
+    public WebElement iframes() {
+        return driver.findElement(iframes);
+    }
+    public WebElement loader() {
+        return driver.findElement(loader);
+    }
+    public WebElement loader_Two() {
+        return driver.findElement(loader_Two);
+    }
+}
+
+
